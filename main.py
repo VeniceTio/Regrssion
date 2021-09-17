8220bcd7d730a8d1b79b8bdd9329c8c6619ebf03
venice <yoann.louazel@gmail.com>
2021-09-17 19:46:26 +0200

    fractorisation gradSimple et gradOpti

--- main.py
+++ main.py
@@ -3,62 +3,41 @@ import sys
 
 
 def gradSimple(p_exp, ppas, ppt, tolerance):
-    variables = p_exp.free_symbols
-    grad = []
-    XK1 = []
-    vec = []
-    for var in variables:
-        XK1.append(0)
-        grad.append(p_exp.diff(var))
-    print(variables)
-    print(ppt)
-    vec = list(zip(variables, ppt))
-    print(vec)
+    variables, size, p, grad, vec = initForGrad(p_exp, ppt)
     cond = Matrix(grad).subs(vec).norm()
-    for i in range(len(XK1)):
-        XK1[i] = (ppt[i] - ppas * ((grad[i].subs(vec))/cond)).evalf()
+    XK1 = Xk(vec, ppas, grad, size, pmod=1, pcond=cond)
     print(XK1)
-    u = Matrix(XK1)
-    cond = u.norm()
-    j = 0
+    cond = Matrix(XK1).norm()
     while cond > tolerance:
-        for i in range(len(XK1)):
-            XK1[i] = (vec[i][1] - ppas * (grad[i].subs(vec)/cond)).evalf()
+        XK1 =Xk(vec, ppas, grad, size, pmod=1, pcond=cond)
         vec = list(zip(variables, XK1))
-        print(vec)
-        u = Matrix(XK1)
-        cond = u.norm()
-        print(cond)
-        j += 1
-    print(XK1)
+        cond = Matrix(XK1).norm()
     return XK1
 
 
 def gradPOpti(p_exp, ppt, tolerance):
-    variables = p_exp.free_symbols
-    size = len(variables)
-    grad = []
-    for var in variables:
-        grad.append(p_exp.diff(var))
-    vec = list(zip(variables, ppt))
-    expas = expPas(ppt, grad, vec, size) # debut du calcul du pas opti TODO: selction du choix parmi ceux sortie
-    vec2 = list(zip(variables, expas))
-    p = Symbol('p')
-    pas = solve(p_exp.subs(vec2), p)         #fin calcul du pas opti
+    variables, size, p, grad, vec = initForGrad(p_exp, ppt)
+    expas = expPas(ppt, grad, vec, size) # debut du calcul du pas opti
+    pas = pasOpti(p_exp, list(zip(variables, expas)), p)
     XK1 = Xk(vec, pas, grad, size)
-    vec = list(zip(variables, XK1))
-    u = Matrix(XK1)                          #calcul condition d'arret
-    cond = u.norm()
-    while cond > tolerance:
-        expas = expPas(ppt, grad, vec, size)
-        vec2 = list(zip(variables, expas))
-        pas = solve(p_exp.subs(vec2), p)
-        XK1 = Xk(vec, pas, grad, size)
+    cond = Matrix(XK1).norm()
+    while cond > tolerance:                     #calcul condition d'arret
         vec = list(zip(variables, XK1))
-        u = Matrix(XK1)
-        cond = u.norm()
+        expas = expPas(ppt, grad, vec, size)
+        pas = pasOpti(p_exp, list(zip(variables, expas)), p)
+        XK1 = Xk(vec, pas, grad, size)
+        cond = Matrix(XK1).norm()
     return XK1
 
+
+def initForGrad(pexp, ppoint):
+    variables = pexp.free_symbols
+    return variables, len(variables), Symbol('p'), [pexp.diff(var) for var in variables], list(zip(variables, ppoint))
+
+
+def pasOpti(pexprpas, pvec, pp): #TODO: selction du choix parmi ceux sortie
+    pas = solve(pexprpas.subs(pvec), pp)         #fin calcul du pas opti
+    return pas
 
 def expPas(ppnt, pgrad, pvec, pdim):
     """
@@ -75,7 +54,7 @@ def expPas(ppnt, pgrad, pvec, pdim):
     return [parse_expr(str(ppnt[i]) + " - p * " + str(pgrad[i].subs(pvec))) for i in range(pdim)]
 
 
-def Xk(pvec, ppas, pgrad, pdim):
+def Xk(pvec, ppas, pgrad, pdim, pmod=0, pcond=1):
     """
     renvoie le point X k+1 pour une regression
     :param pvec: [(var1, val1), ...,(varn, valn)] tableau qui associe au valeur de ppnt le nom de la variable à laquel
@@ -84,7 +63,11 @@ def Xk(pvec, ppas, pgrad, pdim):
     :param pdim: n dimension du vecteur de la fonction
     :return: [val1, .. , valn] point à l'iteration k+1 soit Xk+1
     """
-    return [pvec[i][1] - ppas[0] * pgrad[i].subs(pvec) for i in range(pdim)]
+    if pmod == 0:
+        res = [pvec[i][1] - ppas[0] * pgrad[i].subs(pvec) for i in range(pdim)]
+    else:
+        res = [(pvec[i][1] - ppas * (pgrad[i].subs(pvec)/pcond)).evalf() for i in range(pdim)]
+    return res
 
 
 if __name__ == '__main__':
@@ -92,7 +75,7 @@ if __name__ == '__main__':
         print("Usage : python main.py expression pas")
     else:
         f = parse_expr(sys.argv[1])
-        res = gradSimple(f, 0.25, [10, 10], 0.5)
+        res = gradPOpti(f, [10, 10], 0.5)
         variables = f.free_symbols
         vec = list(zip(variables, res))
 
