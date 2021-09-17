89550068b3183f72c7f007c53985907dcd6c5d99
venice <yoann.louazel@gmail.com>
2021-09-17 22:23:13 +0200

    ajout du mode verbose a gradPOpti et gradSimple

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sympy import *
 import sys
 
 
-def gradSimple(p_exp, ppas, ppt, tolerance):
+def gradSimple(p_exp, ppas, ppt, tolerance, pverbose=0):
     """
     Effectue une descente de gradient simple a pas fixe
     :param p_exp: expression de la fonction sur laquel effectuer une descente de gradient
@@ -11,28 +11,32 @@ def gradSimple(p_exp, ppas, ppt, tolerance):
     :param ppt: [val1, .. , valn] point de depart de la descente de gradient
     :param tolerance: float seuil à partir duquel on decidra que l'aproximation est suffisante (par rapport a la norme
 du point trouvé
+    :param pverbose: int par defaut à 0 et si different de 0 alors le mode verbose est activé
     :return: [val1, .. , valn] point au plus proche du minimum local
     """
     variables, size, p, grad, vec = initForGrad(p_exp, ppt)
     cond = Matrix(grad).subs(vec).norm()
     XK1 = Xk(vec, ppas, grad, size, pmod=1, pcond=cond)
     cond = Matrix([grad[i].subs(vec) for i in range(size)]).norm()
-    print(cond)
+    j = 1
     while cond > tolerance:
+        if pverbose == 1:
+            print("X{} : {}".format(j, XK1))
+            j += 1
         XK1 = Xk(vec, ppas, grad, size, pmod=1, pcond=cond)
         vec = list(zip(variables, XK1))
         cond = Matrix([grad[i].subs(vec) for i in range(size)]).norm()
-        print(cond)
     return XK1
 
 
-def gradPOpti(p_exp, ppt, tolerance):
+def gradPOpti(p_exp, ppt, tolerance, pverbose=0):
     """
     Effectue une descente de gradient simple a pas optimisé
     :param p_exp: expression de la fonction sur laquel effectuer une descente de gradient
     :param ppt: [val1, .. , valn] point de depart de la descente de gradient
     :param tolerance: float seuil à partir duquel on decidra que l'aproximation est suffisante (par rapport a la norme
 du point trouvé
+    :param pverbose: int par defaut à 0 et si different de 0 alors le mode verbose est activé
     :return: [val1, .. , valn] point au plus proche du minimum local
     """
     variables, size, p, grad, vec = initForGrad(p_exp, ppt)
@@ -40,7 +44,11 @@ du point trouvé
     pas = pasOpti(p_exp, list(zip(variables, expas)), p)
     XK1 = Xk(vec, pas, grad, size)
     cond = Matrix([grad[i].subs(vec) for i in range(size)]).norm()
+    j = 1
     while cond > tolerance:
+        if pverbose == 1:
+            print("X{} : {}".format(j, XK1))
+            j += 1
         vec = list(zip(variables, XK1))
         expas = expPas(ppt, grad, vec, size)
         pas = pasOpti(p_exp, list(zip(variables, expas)), p)
@@ -141,6 +149,11 @@ def Xk(pvec, ppas, pgrad, pdim, pmod=0, pcond=1):
 
 
 def printUsage(poption: string):
+    """
+    construit la phrase renvoyé en cas d'erreur de saisi
+    :param poption: option d'usage saisie
+    :return: chaine renvoyé spécifique à chaque option possible
+    """
     chaine = "Usage : python main.py \"expression\" " + poption + "tolerance point"
     if poption == "-S":
         chaine += " pas"
@@ -153,19 +166,24 @@ def printUsage(poption: string):
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage : python main.py \"expression\" <arg>")
+        print("Usage : python main.py \"expression\" <arg> \n - argument possible: -S -O -F -P")
     else:
         res = -1
+        ver = 0
         f = parse_expr(sys.argv[1])
         if sys.argv[2] == "-S":
-            if len(sys.argv) == 6:
+            if len(sys.argv) == 6 or (len(sys.argv) == 7 and sys.argv[6] == "-v"):
+                if sys.argv[-1] == "-v":
+                    ver = 1
                 res = gradSimple(f, float(sys.argv[5]), list(map(float, list(sys.argv[4].split(" ")))),
-                                 float(sys.argv[3]))
+                                 float(sys.argv[3]), ver)
             else:
                 print(printUsage(sys.argv))
         elif sys.argv[2] == "-O":
-            if len(sys.argv) == 5:
-                res = gradPOpti(f, list(map(float, list(sys.argv[4].split(" ")))), float(sys.argv[3]))
+            if len(sys.argv) == 5 or (len(sys.argv) == 6 and sys.argv[6] == "-v"):
+                if sys.argv[-1] == "-v":
+                    ver = 1
+                res = gradPOpti(f, list(map(float, list(sys.argv[4].split(" ")))), float(sys.argv[3]), ver)
             else:
                 print(printUsage(sys.argv))
         elif sys.argv[2] == "-F":
